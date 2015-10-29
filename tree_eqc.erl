d9dec9d2b8d0d3bbe2995e2ad457c4966beef1e4
Thomas Arts <thomas.arts@quviq.com>
2015-10-29 19:08:13 +0100

    Add logic for tree in model

--- tree_eqc.erl
+++ tree_eqc.erl
@@ -6,6 +6,58 @@
 initial_state() ->
   [].
 
+%% insert
+
+insert_args(S) ->
+  [key(S),int()].
+
+%% insert_pre(S,[K,V]) ->
+%%   not lists:keymember(K,1,S).
+
+insert(K,V) ->
+  tree:insert(K,V).
+
+insert_next(S,_,[K,V]) ->
+  lists:keystore(K,1,S,{K,V}).
+
+%% lookup
+
+lookup_args(S) ->
+  [key(S)].
+
+lookup(K) ->
+  tree:lookup(K).
+
+lookup_post(S,[K],Res) ->
+  eq(Res,proplists:get_value(K,S,-1)).
+
+%% update
+
+update_args(S) ->
+  [key(S),int()].
+
+update_pre(S,[K,_]) ->
+  lists:keymember(K,1,S).
+
+update(K,V) ->
+  tree:update(K,V).
+
+update_next(S,_,[K,V]) ->
+  lists:keystore(K,1,S,{K,V}).
+
+%% delete
+
+delete_args(S) ->
+  [key(S)].
+
+delete_pre(S,[K]) ->
+  lists:keymember(K,1,S).
+
+delete(K) ->
+  tree:delete(K).
+
+delete_next(S,_,[K]) ->
+  lists:keydelete(K,1,S).
 
 %% generators
 
@@ -19,7 +71,7 @@ key(S) ->
 prop_tree() ->
   ?SETUP(fun() ->
              compile(),
-             fun() -> cleanup()  %% must stop eqc_c in order to not get error "tree.gcda:stamp mismatch with notes file"
+             fun() -> cleanup() 
              end
          end,
   ?FORALL(Cmds, commands(?MODULE),
@@ -33,13 +85,8 @@ prop_tree() ->
 
 
 compile() ->
-  eqc_c_cover_start(tree).
-
-eqc_c_cover_start(File) ->
-  eqc_c:start(File, [definitions_only, {cflags, "-coverage"}]).
+  eqc_c:start(tree, [definitions_only, {cflags, "-coverage"}]).
 
 cleanup() ->
-  eqc_c_cover_stop().
-
-eqc_c_cover_stop() ->
   eqc_c:stop().
+
